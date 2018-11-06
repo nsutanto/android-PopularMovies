4b437ea9704a40dbf96bd704f36c657cbb660cbb
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-06 15:20:02 -0600

    Add review model

---
+++ app/src/main/java/com/nsutanto/popularmovies/data/model/Review.kt
@@ -0,0 +1,8 @@
+package com.nsutanto.popularmovies.data.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Review(val author: String,
+                  val content: String) : Parcelable
