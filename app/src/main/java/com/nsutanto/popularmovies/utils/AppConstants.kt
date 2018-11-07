7dcf39689eadad5c5241af7fb1f2d78285dad134
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-07 09:33:36 -0600

    Add app constant

--- app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
+++ app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
@@ -1,5 +1,11 @@
 package com.nsutanto.popularmovies.utils
 
+import com.nsutanto.popularmovies.BuildConfig
+
 object AppConstants {
 
+    const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/w185"
+    const val BASE_URL = "https://api.themoviedb.org/3"
+    const val API_KEY = "api_key"
+    const val API_KEY_VALUE = BuildConfig.API_KEY
 }
