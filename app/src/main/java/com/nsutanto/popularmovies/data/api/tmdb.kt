2a67e7d8777ce1eb0098ceb7858bdc70cc461732
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-07 10:02:43 -0600

    Add videos and reviews

--- app/src/main/java/com/nsutanto/popularmovies/data/api/tmdb.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/tmdb.kt
@@ -1,7 +1,10 @@
 package com.nsutanto.popularmovies.data.api
 
 import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.data.model.Review
+import com.nsutanto.popularmovies.data.model.Video
 import retrofit2.http.GET
+import retrofit2.http.Path
 import rx.Observable
 
 interface tmdb {
@@ -11,4 +14,10 @@ interface tmdb {
 
     @GET("movie/top_rated")
     fun getTopRatedMovie(): Observable<Movie>
+
+    @GET("movie/{id}/videos")
+    fun getVideos(@Path("id") movieID: String): Observable<Video>
+
+    @GET("movie/{id}/reviews")
+    fun getReviews(@Path("id") movieID: String): Observable<Review>
 }
