360c42add2825c53438fbba571f75a44c1dbcdbb
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-09 16:01:30 -0600

    setup movie adapter

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MovieAdapter.kt
@@ -0,0 +1,53 @@
+package com.nsutanto.popularmovies.ui.main
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.nsutanto.popularmovies.R
+import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.utils.AppConstants
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.movie_item.view.*
+
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    private var movies = listOf<Movie>()
+
+    inner class MovieViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        val posterView: ImageView = v.iv_poster
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            //mTaskListener.StartDetailActivity(movies.get(adapterPosition))
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+
+        val posterPath = movies[position].posterPath
+        if (posterPath != "") {
+            Picasso.get()
+                .load(AppConstants.BASE_URL_POSTER + posterPath)
+                .into(holder.posterView)
+        }
+    }
+
+    override fun getItemCount() = movies.size
+
+    fun setMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+}
