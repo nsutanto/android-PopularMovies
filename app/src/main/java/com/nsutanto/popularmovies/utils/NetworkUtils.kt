a500a1a98d740f8cc9f81d837a734ae4db804485
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-09 16:01:41 -0600

    Improve network utils

--- app/src/main/java/com/nsutanto/popularmovies/utils/NetworkUtils.kt
+++ app/src/main/java/com/nsutanto/popularmovies/utils/NetworkUtils.kt
@@ -7,10 +7,7 @@ object NetworkUtils {
 
     fun isNetworkConnected(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (cm != null) {
-            val activeNetwork = cm.activeNetworkInfo
-            return activeNetwork != null && activeNetwork.isConnectedOrConnecting
-        }
-        return false
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
     }
 }// This class is not publicly instantiable
