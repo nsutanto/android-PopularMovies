0da65c5021f58265b0effab6e9cadfb99654ab4c
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-09 16:01:08 -0600

    setup recycler view

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
@@ -2,12 +2,30 @@ package com.nsutanto.popularmovies.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.nsutanto.popularmovies.R
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewAdapter: MovieAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+        //val vm = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        //vm.getUsers().observe(this, Observer<List<User>>{ users ->
+            // update UI
+        //})
+    }
+
+    private fun setupRecyclerView() {
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MovieAdapter()
+        rv_movie.adapter = viewAdapter
+        rv_movie.layoutManager = viewManager
     }
 }
