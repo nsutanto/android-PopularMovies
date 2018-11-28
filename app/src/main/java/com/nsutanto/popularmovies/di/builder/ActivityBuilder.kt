1a6dfda7d0ea972e249b6f2b4f3dc009ba3580ec
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-28 11:08:56 -0600

    Implement DI - Activity Builder

---
+++ app/src/main/java/com/nsutanto/popularmovies/di/builder/ActivityBuilder.kt
@@ -0,0 +1,12 @@
+package com.nsutanto.popularmovies.di.builder
+
+import com.nsutanto.popularmovies.ui.main.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBuilder {
+
+    //@ContributesAndroidInjector(modules = [(MainActivityModule::class)])
+    //abstract fun bindMainActivity(): MainActivity
+}
