b92c94c62318c4d3997d32576a52bd4c60360389
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2018-11-28 11:08:40 -0600

    Remove View Model

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.nsutanto.popularmovies.data.model.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +12,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewAdapter: MovieAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupRecyclerView()
-        //val vm = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        //vm.getUsers().observe(this, Observer<List<User>>{ users ->
-            // update UI
-        //})
     }
 
     private fun setupRecyclerView() {
