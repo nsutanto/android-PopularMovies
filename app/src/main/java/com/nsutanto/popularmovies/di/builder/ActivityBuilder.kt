974e5732afd2f5992b010df7ffe79f3061bcc8ce
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-23 16:35:10 -0600

    add activity builder

--- app/src/main/java/com/nsutanto/popularmovies/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/nsutanto/popularmovies/di/builder/ActivityBuilder.kt
@@ -1,12 +1,13 @@
 package com.nsutanto.popularmovies.di.builder
 
 import com.nsutanto.popularmovies.ui.main.MainActivity
+import com.nsutanto.popularmovies.ui.main.MainActivityModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilder {
 
-    //@ContributesAndroidInjector(modules = [(MainActivityModule::class)])
-    //abstract fun bindMainActivity(): MainActivity
+    @ContributesAndroidInjector(modules = [(MainActivityModule::class)])
+    abstract fun bindMainActivity(): MainActivity
 }
