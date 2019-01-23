54cfd1bcd0c30946c5d5f6218e46377c2adb785c
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-23 16:39:42 -0600

    create main presenter

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainPresenter.kt
@@ -0,0 +1,36 @@
+package com.nsutanto.popularmovies.ui.main
+
+import com.nsutanto.popularmovies.data.api.ApiSource
+import com.nsutanto.popularmovies.data.model.PopularMovieResult
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import timber.log.Timber
+import javax.inject.Inject
+
+class MainPresenter
+@Inject
+constructor(val view: MainContract.View,
+            val api: ApiSource
+) : MainContract.Presenter {
+
+    private var apiRequest = CompositeDisposable()
+
+    override fun start() {
+
+        apiRequest.add(api.getPopularMovies()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ movies -> handleGetPopularMovies(movies) }, { e -> handleNetworkError(e) }))
+    }
+
+    override fun stop() {
+
+    }
+
+    private fun handleGetPopularMovies(movies: PopularMovieResult) {
+
+    }
+
+    private fun handleNetworkError(e: Throwable) {
+        Timber.e(e)
+    }
+}
