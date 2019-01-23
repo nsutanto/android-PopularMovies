e0746c0d86cc2cc96d143b37de393a92e0f11ba9
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-23 16:34:36 -0600

    add api factory

--- app/src/main/java/com/nsutanto/popularmovies/data/api/APIFactory.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/APIFactory.kt
@@ -4,10 +4,12 @@ import com.nsutanto.popularmovies.utils.AppConstants
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class APIFactory {
+class APIFactory @Inject
+constructor() {
 
     fun createTMDBService(): TMDBService {
         val retrofit = Retrofit.Builder()
