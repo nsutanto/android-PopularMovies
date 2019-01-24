3441fdda2ccf92656b6b55327ca407dabd42d92c
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-24 15:52:37 -0600

    update api factory

--- app/src/main/java/com/nsutanto/popularmovies/data/api/APIFactory.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/APIFactory.kt
@@ -1,6 +1,8 @@
 package com.nsutanto.popularmovies.data.api
 
 import com.nsutanto.popularmovies.utils.AppConstants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,13 +13,27 @@ import javax.inject.Singleton
 class APIFactory @Inject
 constructor() {
 
+    private lateinit var client: OkHttpClient
+
     fun createTMDBService(): TMDBService {
         val retrofit = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(AppConstants.BASE_URL)
+            .client(createHttpClient())
             .build()
 
         return retrofit.create(TMDBService::class.java)
+    }
+
+    private fun createHttpClient(): OkHttpClient {
+
+        client = OkHttpClient()
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        return client.newBuilder()
+            .addInterceptor(logging)
+            .addInterceptor(AuthenticationInterceptor())
+            .build()
     }
 }
