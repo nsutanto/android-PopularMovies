a702f346873ea118d62a1311b15dd7b3482e6bc0
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-24 07:38:51 -0600

    add main contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainContract.kt
@@ -0,0 +1,10 @@
+package com.nsutanto.popularmovies.ui.main
+
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+
+interface MainContract {
+    interface View
+
+    interface Presenter : BasePresenter
+}
+
