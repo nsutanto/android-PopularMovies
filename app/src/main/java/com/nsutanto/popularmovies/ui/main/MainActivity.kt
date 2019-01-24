e26fd8e49671378bf2a94e3bc9cdcd21f6010e49
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-24 07:39:02 -0600

    add main activity

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.nsutanto.popularmovies.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.R
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+
+    @Inject
+    lateinit var presenter: MainContract.Presenter
 
     private lateinit var viewAdapter: MovieAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setupRecyclerView()
+        //setupRecyclerView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.start()
     }
 
     private fun setupRecyclerView() {
