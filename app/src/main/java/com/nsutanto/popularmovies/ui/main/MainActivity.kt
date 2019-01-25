19b84a592780210d25b6b6c72b112ddf972add17
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-25 07:39:42 -0600

    udpate main activity

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.nsutanto.popularmovies.ui.main
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.nsutanto.popularmovies.R
+import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.ui.base.view.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import android.content.Context
+import android.support.v7.widget.GridLayoutManager
+
 
 class MainActivity : BaseActivity(), MainContract.View {
 
@@ -20,7 +23,7 @@ class MainActivity : BaseActivity(), MainContract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setupRecyclerView()
+        setupRecyclerView()
     }
 
     override fun onStart() {
@@ -28,10 +31,26 @@ class MainActivity : BaseActivity(), MainContract.View {
         presenter.start()
     }
 
+    override fun displayMovies(movies: List<Movie>) {
+        viewAdapter.setMovies(movies)
+    }
+
     private fun setupRecyclerView() {
-        viewManager = LinearLayoutManager(this)
+
+        val numColumn = calculateNoOfColumns(this)
+        viewManager = GridLayoutManager(this, numColumn)
         viewAdapter = MovieAdapter()
         rv_movie.adapter = viewAdapter
         rv_movie.layoutManager = viewManager
+    }
+
+    private fun calculateNoOfColumns(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        val dpWidth = displayMetrics.widthPixels / displayMetrics.density
+        val scalingFactor = 200
+        var noOfColumns = (dpWidth / scalingFactor).toInt()
+        if (noOfColumns < 2)
+            noOfColumns = 2
+        return noOfColumns
     }
 }
