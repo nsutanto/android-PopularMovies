aff1e097d7ac1fa11dce434e73b18b3c5eace60c
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-25 09:20:45 -0600

    api repository

--- app/src/main/java/com/nsutanto/popularmovies/data/api/APIRepository.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/APIRepository.kt
@@ -1,16 +1,56 @@
 package com.nsutanto.popularmovies.data.api
 
-import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.data.model.MovieResponse
+import com.nsutanto.popularmovies.data.model.Review
+import com.nsutanto.popularmovies.data.model.Video
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class APIRepository
-constructor(private val apiFactory: APIFactory) {
+@Inject
+constructor(private val apiFactory: APIFactory) : ApiSource {
 
-    fun getPopularMovies(): Observable<Movie> {
-        val tmdbService = apiFactory.createTMDBService()
+    private var tmdbService = apiFactory.createTMDBService()
+
+    override fun getPopularMovies(): Observable<MovieResponse> {
 
         return tmdbService.getPopularMovie()
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getTopRatedMovies(): Observable<MovieResponse> {
+        return tmdbService.getTopRatedMovie()
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getMovieVideos(id: String): Observable<Video> {
+       return tmdbService.getMovieVideos(id)
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getMovieReviews(id: String): Observable<Review> {
+        return tmdbService.getMovieReviews(id)
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getPopularTV(): Observable<MovieResponse> {
+        return tmdbService.getPopularTV()
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getTopRatedTV(): Observable<MovieResponse> {
+        return tmdbService.getTopRatedTV()
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getTVVideos(id: String): Observable<Video> {
+        return tmdbService.getTVVideos(id)
+            .subscribeOn(Schedulers.newThread())
+    }
+
+    override fun getTVReviews(id: String): Observable<Review> {
+        return tmdbService.getTVReviews(id)
             .subscribeOn(Schedulers.newThread())
     }
 }
