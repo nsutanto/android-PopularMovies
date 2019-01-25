a3cfbcf4fc3812a7a65f36bdb19d6e2c0cec7320
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-25 09:20:53 -0600

    api source

--- app/src/main/java/com/nsutanto/popularmovies/data/api/APISource.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/APISource.kt
@@ -1,9 +1,26 @@
 package com.nsutanto.popularmovies.data.api
 
 import com.nsutanto.popularmovies.data.model.MovieResponse
+import com.nsutanto.popularmovies.data.model.Review
+import com.nsutanto.popularmovies.data.model.Video
 import io.reactivex.Observable
 
 interface ApiSource {
 
     fun getPopularMovies(): Observable<MovieResponse>
+
+    fun getTopRatedMovies(): Observable<MovieResponse>
+
+    fun getMovieVideos(id: String): Observable<Video>
+
+    fun getMovieReviews(id: String): Observable<Review>
+
+    fun getPopularTV(): Observable<MovieResponse>
+
+    fun getTopRatedTV(): Observable<MovieResponse>
+
+    fun getTVVideos(id: String): Observable<Video>
+
+    fun getTVReviews(id: String): Observable<Review>
+
 }
