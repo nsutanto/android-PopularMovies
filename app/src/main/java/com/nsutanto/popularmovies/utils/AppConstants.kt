1cf563af0fbadc1a400406011c2814f8b36d7dd8
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-01-28 08:27:31 -0600

    fix app constant

--- app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
+++ app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
@@ -4,8 +4,8 @@ import com.nsutanto.popularmovies.BuildConfig
 
 object AppConstants {
 
-    const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/w185"
-    const val BASE_URL = "https://api.themoviedb.org/3"
+    const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/w185/"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
     const val YOUTUBE_THUMBNAIL_PRE = "http://img.youtube.com/vi/"
     const val YOUTUBE_THUMBNAIL_POST = "/0.jpg"
