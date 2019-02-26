a201da0034dbc8684b2a1754a52cda8770574e08
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-02-26 15:17:52 -0600

    create tv presenter

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVPresenter.kt
@@ -0,0 +1,21 @@
+package com.nsutanto.popularmovies.ui.main.tv
+
+import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.ui.main.MainActivityListener
+import javax.inject.Inject
+
+class TVPresenter @Inject
+constructor() : TVContract.Presenter, MainActivityListener {
+
+    override fun start() {
+    }
+
+    override fun stop() {
+
+    }
+
+    // Main Activity Listener
+    override fun onPopularMovieUpdate(movies: List<Movie>) {
+
+    }
+}
