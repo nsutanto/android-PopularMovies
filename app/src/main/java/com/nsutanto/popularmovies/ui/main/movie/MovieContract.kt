5f537186c6543c7245c3b4c3e245b79d7de1976a
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-02-27 08:50:04 -0600

    create movie contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieContract.kt
@@ -0,0 +1,10 @@
+package com.nsutanto.popularmovies.ui.main.movie
+
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+
+class MovieContract {
+
+    interface View
+
+    interface Presenter : BasePresenter
+}
