025ba4952a9bb4caeb3a07989e1c04928e4fe993
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-02-27 08:50:50 -0600

    create tv contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVContract.kt
@@ -0,0 +1,10 @@
+package com.nsutanto.popularmovies.ui.main.tv
+
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+
+class TVContract {
+
+    interface View
+
+    interface Presenter : BasePresenter
+}
