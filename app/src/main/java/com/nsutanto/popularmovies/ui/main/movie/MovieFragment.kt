07c89189df726d04074ec94dd98fab36842022a7
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-02-28 07:40:58 -0600

    update movie fragment

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.nsutanto.popularmovies.R
+import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.ui.base.view.BaseFragment
+import com.nsutanto.popularmovies.viewmodels.MovieViewModel
 import javax.inject.Inject
 
 class MovieFragment : BaseFragment(), MovieContract.View {
@@ -14,8 +18,22 @@ class MovieFragment : BaseFragment(), MovieContract.View {
     @Inject
     lateinit var presenter: MoviePresenter
 
+    private lateinit var vm: MovieViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*
+        vm = activity?.run {
+            ViewModelProviders.of(this).get(MovieViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+        vm.getMovies().observe(this, Observer<List<Movie>> { movies ->
+            // Update the UI
+        })
+        */
+
+
+
         /*
         arguments?.let {
             loanInfo = it.getParcelable(LOAN_INFO)
@@ -26,10 +44,7 @@ class MovieFragment : BaseFragment(), MovieContract.View {
         */
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie, container, false)
     }
@@ -53,4 +68,10 @@ class MovieFragment : BaseFragment(), MovieContract.View {
         super.onAttach(context)
         //normalFragmentListener = context as NormalFragmentListener
     }
+
+    fun updateMovies(movies: List<Movie>) {
+
+    }
+
+
 }
