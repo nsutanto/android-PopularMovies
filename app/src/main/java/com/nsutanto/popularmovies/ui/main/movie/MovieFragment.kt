80667ae39d557cdb400e8b1ff4b36ad7ea15080e
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-04 12:45:00 -0600

    update movie fragment

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
@@ -1,6 +1,5 @@
 package com.nsutanto.popularmovies.ui.main.movie
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +10,6 @@ import com.nsutanto.popularmovies.R
 import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.ui.base.view.BaseFragment
 import com.nsutanto.popularmovies.ui.main.MovieAdapter
-import com.nsutanto.popularmovies.viewmodels.MovieViewModel
 import kotlinx.android.synthetic.main.fragment_movie.*
 import javax.inject.Inject
 
@@ -20,35 +18,8 @@ class MovieFragment : BaseFragment(), MovieContract.View {
     @Inject
     lateinit var presenter: MoviePresenter
 
-    private lateinit var vm: MovieViewModel
-
     private lateinit var popularMoviesAdapter: MovieAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setPopularMoviesRV()
-
-        /*
-        vm = activity?.run {
-            ViewModelProviders.of(this).get(MovieViewModel::class.java)
-        } ?: throw Exception("Invalid Activity")
-        vm.getMovies().observe(this, Observer<List<Movie>> { movies ->
-            // Update the UI
-        })
-        */
-
-
-
-        /*
-        arguments?.let {
-            loanInfo = it.getParcelable(LOAN_INFO)
-            borrower = it.getParcelable(BORROWER)
-            userFlowData = it.getParcelable(USER_FLOW)
-
-        }
-        */
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -57,6 +28,7 @@ class MovieFragment : BaseFragment(), MovieContract.View {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setPopularMoviesRV()
     }
 
     override fun onStart() {
@@ -69,15 +41,8 @@ class MovieFragment : BaseFragment(), MovieContract.View {
         presenter.stop()
     }
 
-    /*
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        //normalFragmentListener = context as NormalFragmentListener
-    }
-    */
-
     fun updatePopularMovies(movies: List<Movie>) {
-        //popularMoviesAdapter.setMovies(movies)
+        popularMoviesAdapter.setMovies(movies)
     }
 
     private fun setPopularMoviesRV() {
