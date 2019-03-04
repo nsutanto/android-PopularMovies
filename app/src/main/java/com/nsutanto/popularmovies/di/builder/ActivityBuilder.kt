3e416e7c837d05c65dc4e13533760237b9e3fb15
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-04 11:07:33 -0600

    update activity builder

--- app/src/main/java/com/nsutanto/popularmovies/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/nsutanto/popularmovies/di/builder/ActivityBuilder.kt
@@ -2,12 +2,32 @@ package com.nsutanto.popularmovies.di.builder
 
 import com.nsutanto.popularmovies.ui.main.MainActivity
 import com.nsutanto.popularmovies.ui.main.MainActivityModule
+import com.nsutanto.popularmovies.ui.main.movie.MovieFragment
+import com.nsutanto.popularmovies.ui.main.movie.MovieFragmentModule
+import com.nsutanto.popularmovies.ui.main.tv.TVFragment
+import com.nsutanto.popularmovies.ui.main.tv.TVFragmentModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
-abstract class ActivityBuilder {
+abstract class MainFragmentsModule {
+    @ContributesAndroidInjector(modules = [MovieFragmentModule::class])
+    abstract fun movieFragment(): MovieFragment
 
-    @ContributesAndroidInjector(modules = [(MainActivityModule::class)])
-    abstract fun bindMainActivity(): MainActivity
+    @ContributesAndroidInjector(modules = [TVFragmentModule::class])
+    abstract fun tvFragment(): TVFragment
 }
+
+
+
+@Module
+interface ActivityBuilder {
+
+    @ContributesAndroidInjector(modules =
+    [
+        MainActivityModule::class,
+        MainFragmentsModule::class
+    ])
+    fun mainActivity(): MainActivity
+}
+
