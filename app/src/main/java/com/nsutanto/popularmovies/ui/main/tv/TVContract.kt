3c8e6baf1ab379909d2806739b4e7a788e30720d
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-06 07:41:45 -0600

    update tv contract

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVContract.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVContract.kt
@@ -1,10 +1,19 @@
 package com.nsutanto.popularmovies.ui.main.tv
 
+import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
 
 class TVContract {
 
-    interface View
+    interface View {
+        fun displayPopularTV(movies: List<Movie>?)
 
-    interface Presenter : BasePresenter
+        fun displayTopRatedTV(movies: List<Movie>?)
+    }
+
+    interface Presenter : BasePresenter {
+        fun onUpdatePopularTV(movies: List<Movie>?)
+
+        fun onUpdateTopRatedTV(movies: List<Movie>?)
+    }
 }
