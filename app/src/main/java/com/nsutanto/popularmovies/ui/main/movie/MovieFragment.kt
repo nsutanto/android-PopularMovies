514c8e920e7f71619c5be901ad9f17f44d9b14a3
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-07 07:38:09 -0600

    udpate movie fragment

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieFragment.kt
@@ -9,16 +9,14 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nsutanto.popularmovies.R
-import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.data.model.MovieResponse
 import com.nsutanto.popularmovies.ui.base.view.BaseFragment
 import com.nsutanto.popularmovies.ui.main.MovieAdapter
+import com.nsutanto.popularmovies.utils.AppConstants.INVALID_ACTIVITY
 import com.nsutanto.popularmovies.viewmodel.MainViewModel
 import com.nsutanto.popularmovies.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_movie.*
 import javax.inject.Inject
-
-
 
 class MovieFragment : BaseFragment(), MovieContract.View {
 
@@ -29,25 +27,13 @@ class MovieFragment : BaseFragment(), MovieContract.View {
     lateinit var factory: ViewModelFactory
 
     private lateinit var mainViewModel: MainViewModel
-    private var popularMovies: MovieResponse? = null
-    private var topRatedMovies: MovieResponse? = null
     private lateinit var popularMoviesAdapter: MovieAdapter
     private lateinit var topRatedMoviesAdapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mainViewModel = activity?.run {
-            ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
-        } ?: throw Exception("Invalid Activity")
-
-        mainViewModel.popularMovies.observe(this, Observer<MovieResponse> {
-                movies -> updatePopularMovies(movies)
-        })
-
-        mainViewModel.topRatedMovies.observe(this, Observer<MovieResponse> {
-                movies -> updateTopRatedMovies(movies)
-        })
+        setViewModel()
+        createAdapters()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -57,10 +43,7 @@ class MovieFragment : BaseFragment(), MovieContract.View {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setPopularMoviesRV()
-        setTopRatedMoviesRV()
-
-        popularMovies?.let { updatePopularMovies(popularMovies!!)}
+        setRecyclerView()
     }
 
     override fun onStart() {
@@ -71,32 +54,44 @@ class MovieFragment : BaseFragment(), MovieContract.View {
     override fun onStop() {
         super.onStop()
         presenter.stop()
-        onSaveInstanceState(Bundle())
     }
 
-    fun updatePopularMovies(movieResponse: MovieResponse) {
-        popularMovies = movieResponse
-        popularMoviesAdapter.setMovies(popularMovies?.results)
+    override fun showPopularMovies(movieResponse: MovieResponse) {
+        popularMoviesAdapter.setMovies(movieResponse.results)
         pb_popular_movie.visibility = View.GONE
     }
 
-    fun updateTopRatedMovies(movieResponse: MovieResponse) {
-        topRatedMovies = movieResponse
-        topRatedMoviesAdapter.setMovies(topRatedMovies?.results)
+    override fun showTopRatedMovies(movieResponse: MovieResponse) {
+        topRatedMoviesAdapter.setMovies(movieResponse.results)
         pb_top_rated_movie.visibility = View.GONE
     }
 
-    private fun setPopularMoviesRV() {
-        popularMoviesAdapter = MovieAdapter()
-        val viewManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+    private fun setRecyclerView() {
+        val popularMovieLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
         rv_popular_movie.adapter = popularMoviesAdapter
-        rv_popular_movie.layoutManager = viewManager
+        rv_popular_movie.layoutManager = popularMovieLayoutManager
+
+        val topRatedMovieLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        rv_top_rated_movie.adapter = topRatedMoviesAdapter
+        rv_top_rated_movie.layoutManager = topRatedMovieLayoutManager
     }
 
-    private fun setTopRatedMoviesRV() {
+    private fun createAdapters() {
+        popularMoviesAdapter = MovieAdapter()
         topRatedMoviesAdapter = MovieAdapter()
-        val viewManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-        rv_top_rated_movie.adapter = topRatedMoviesAdapter
-        rv_top_rated_movie.layoutManager = viewManager
+    }
+
+    private fun setViewModel() {
+        mainViewModel = activity?.run {
+            ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
+        } ?: throw Exception(INVALID_ACTIVITY)
+
+        mainViewModel.popularMovies.observe(this, Observer<MovieResponse> {
+                movies -> presenter.onUpdatedPopularMovies(movies)
+        })
+
+        mainViewModel.topRatedMovies.observe(this, Observer<MovieResponse> {
+                movies -> presenter.onUpdatedTopRatedMovies(movies)
+        })
     }
 }
