d54e68652c0a3a28ff357ad20afa2c18f1606304
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-07 07:38:00 -0600

    update movie contract

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieContract.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieContract.kt
@@ -1,10 +1,21 @@
 package com.nsutanto.popularmovies.ui.main.movie
 
+import com.nsutanto.popularmovies.data.model.MovieResponse
 import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
 
 class MovieContract {
 
-    interface View
+    interface View {
 
-    interface Presenter : BasePresenter
+        fun showPopularMovies(movieResponse: MovieResponse)
+
+        fun showTopRatedMovies(movieResponse: MovieResponse)
+    }
+
+    interface Presenter : BasePresenter {
+
+        fun onUpdatedPopularMovies(movieResponse: MovieResponse)
+
+        fun onUpdatedTopRatedMovies(movieResponse: MovieResponse)
+    }
 }
