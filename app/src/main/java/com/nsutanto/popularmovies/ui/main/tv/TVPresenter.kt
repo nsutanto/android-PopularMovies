5644182bb5b3c4f184c859a21308288eed37af43
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-07 07:37:16 -0600

    update tv presenter

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVPresenter.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVPresenter.kt
@@ -1,30 +1,15 @@
 package com.nsutanto.popularmovies.ui.main.tv
 
-import com.nsutanto.popularmovies.data.model.Movie
 import javax.inject.Inject
 
 class TVPresenter @Inject
-constructor(private val view: TVContract.View) : TVContract.Presenter {
-
-    private var popularTV: List<Movie>? = null
-    private var topRatedTV: List<Movie>? = null
+    constructor() : TVContract.Presenter {
 
     override fun start() {
-        view.displayPopularTV(popularTV)
-        view.displayTopRatedTV(topRatedTV)
+
     }
 
     override fun stop() {
 
-    }
-
-    override fun onUpdatePopularTV(movies: List<Movie>?) {
-        popularTV = movies
-        view.displayPopularTV(popularTV)
-    }
-
-    override fun onUpdateTopRatedTV(movies: List<Movie>?) {
-        topRatedTV = movies
-        view.displayTopRatedTV(topRatedTV)
     }
 }
