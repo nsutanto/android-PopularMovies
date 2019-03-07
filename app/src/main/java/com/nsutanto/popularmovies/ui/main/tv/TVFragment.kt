f498246db7e8cd6b13ab7ec7756af504484f9ea7
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-07 07:37:04 -0600

    update tv fragment

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVFragment.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/tv/TVFragment.kt
@@ -9,10 +9,10 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nsutanto.popularmovies.R
-import com.nsutanto.popularmovies.data.model.Movie
 import com.nsutanto.popularmovies.data.model.MovieResponse
 import com.nsutanto.popularmovies.ui.base.view.BaseFragment
 import com.nsutanto.popularmovies.ui.main.MovieAdapter
+import com.nsutanto.popularmovies.utils.AppConstants.INVALID_ACTIVITY
 import com.nsutanto.popularmovies.viewmodel.MainViewModel
 import com.nsutanto.popularmovies.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_tv.*
@@ -23,28 +23,20 @@ class TVFragment : BaseFragment(), TVContract.View {
     @Inject
     lateinit var presenter: TVPresenter
 
-    private lateinit var mainViewModel: MainViewModel
-
     @Inject
     lateinit var factory: ViewModelFactory
 
-    private var popularTVAdapter: MovieAdapter? = null
-    private var topRatedTVAdapter: MovieAdapter? = null
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var popularTVAdapter: MovieAdapter
+    private lateinit var topRatedTVAdapter: MovieAdapter
+
+    private var popularTV: MovieResponse? = null
+    private var topRatedTV: MovieResponse? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mainViewModel = activity?.run {
-            ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
-        } ?: throw Exception("Invalid Activity")
-
-        mainViewModel.popularTV.observe(this, Observer<MovieResponse> {
-                movies -> updatePopularTV(movies)
-        })
-
-        mainViewModel.topRatedTV.observe(this, Observer<MovieResponse> {
-                movies -> updateTopRatedTV(movies)
-        })
+        setViewModel()
+        createAdapters()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -54,8 +46,10 @@ class TVFragment : BaseFragment(), TVContract.View {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setPopularTVRV()
-        setTopRatedTVRV()
+        setRecyclerView()
+
+        popularTV?.let { updatePopularTV(popularTV!!)}
+        topRatedTV?.let { updateTopRatedTV(topRatedTV!!)}
     }
 
     override fun onStart() {
@@ -68,37 +62,44 @@ class TVFragment : BaseFragment(), TVContract.View {
         presenter.stop()
     }
 
-    override fun displayPopularTV(movies: List<Movie>?) {
-        popularTVAdapter?.setMovies(movies)
-    }
-
-    override fun displayTopRatedTV(movies: List<Movie>?) {
-        topRatedTVAdapter?.setMovies(movies)
-    }
-
-    fun updatePopularTV(movieResponse: MovieResponse) {
-        //presenter.onUpdatePopularTV(movies)
-        popularTVAdapter?.setMovies(movieResponse.results)
+    private fun updatePopularTV(movieResponse: MovieResponse) {
+        popularTV = movieResponse
+        popularTVAdapter.setMovies(popularTV?.results)
         pb_popular_tv.visibility = View.GONE
     }
 
-    fun updateTopRatedTV(movieResponse: MovieResponse) {
-        //presenter.onUpdateTopRatedTV(movies)
-        topRatedTVAdapter?.setMovies(movieResponse.results)
+    private fun updateTopRatedTV(movieResponse: MovieResponse) {
+        topRatedTV = movieResponse
+        topRatedTVAdapter.setMovies(topRatedTV?.results)
         pb_top_rated_tv.visibility = View.GONE
     }
 
-    private fun setPopularTVRV() {
-        popularTVAdapter = MovieAdapter()
-        val viewManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+    private fun setRecyclerView() {
+        val vmPopularTV = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
         rv_popular_tv.adapter = popularTVAdapter
-        rv_popular_tv.layoutManager = viewManager
+        rv_popular_tv.layoutManager = vmPopularTV
+
+        val vmTopRatedTV = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        rv_top_rated_tv.adapter = topRatedTVAdapter
+        rv_top_rated_tv.layoutManager = vmTopRatedTV
     }
 
-    private fun setTopRatedTVRV() {
+    private fun createAdapters() {
+        popularTVAdapter = MovieAdapter()
         topRatedTVAdapter = MovieAdapter()
-        val viewManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
-        rv_top_rated_tv.adapter = topRatedTVAdapter
-        rv_top_rated_tv.layoutManager = viewManager
+    }
+
+    private fun setViewModel() {
+        mainViewModel = activity?.run {
+            ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
+        } ?: throw Exception(INVALID_ACTIVITY)
+
+        mainViewModel.popularTV.observe(this, Observer<MovieResponse> {
+                movies -> updatePopularTV(movies)
+        })
+
+        mainViewModel.topRatedTV.observe(this, Observer<MovieResponse> {
+                movies -> updateTopRatedTV(movies)
+        })
     }
 }
