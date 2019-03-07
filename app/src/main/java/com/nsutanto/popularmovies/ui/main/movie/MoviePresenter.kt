ee3b7406d344a672aa119c886554f3e87c789255
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-07 07:38:17 -0600

    update movie presenter

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MoviePresenter.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MoviePresenter.kt
@@ -1,15 +1,30 @@
 package com.nsutanto.popularmovies.ui.main.movie
 
+import com.nsutanto.popularmovies.data.model.MovieResponse
 import javax.inject.Inject
 
 class MoviePresenter @Inject
-constructor() :
-    MovieContract.Presenter {
+    constructor(private val view: MovieContract.View) : MovieContract.Presenter {
+
+    private var popularMovies: MovieResponse? = null
+    private var topRatedMovies: MovieResponse? = null
 
     override fun start() {
+        popularMovies?.let { view.showPopularMovies(popularMovies!!) }
+        topRatedMovies?.let { view.showTopRatedMovies(topRatedMovies!!) }
     }
 
     override fun stop() {
 
+    }
+
+    override fun onUpdatedPopularMovies(movieResponse: MovieResponse) {
+        popularMovies = movieResponse
+        popularMovies?.let { view.showPopularMovies(popularMovies!!)}
+    }
+
+    override fun onUpdatedTopRatedMovies(movieResponse: MovieResponse) {
+        topRatedMovies = movieResponse
+        topRatedMovies?.let { view.showTopRatedMovies(topRatedMovies!!)}
     }
 }
