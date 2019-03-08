203fd789d36ad64472f3cd5b3990f1accf342a60
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-08 07:43:28 -0600

    update activity detail

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieAdapter.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/main/movie/MovieAdapter.kt
@@ -1,9 +1,10 @@
-package com.nsutanto.popularmovies.ui.main
+package com.nsutanto.popularmovies.ui.main.movie
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nsutanto.popularmovies.R
 import com.nsutanto.popularmovies.data.model.Movie
@@ -18,6 +19,7 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     inner class MovieViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
         val posterView: ImageView = v.iv_poster
+        val title: TextView = v.tv_title
 
         init {
             v.setOnClickListener(this)
@@ -36,18 +38,22 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
 
-        val posterPath = movies[position].posterPath
+        val movie = movies[position]
+        val posterPath = movie.posterPath
         if (posterPath != "") {
             Picasso.get()
                 .load(AppConstants.BASE_URL_POSTER + posterPath)
                 .into(holder.posterView)
         }
+        holder.title.text = movie.title
     }
 
     override fun getItemCount() = movies.size
 
-    fun setMovies(movies: List<Movie>) {
-        this.movies = movies
-        notifyDataSetChanged()
+    fun setMovies(movies: List<Movie>?) {
+        movies?.let {
+            this.movies = movies
+            notifyDataSetChanged()
+        }
     }
 }
