6e5fc49e8633af2fb0309ac3b69a6ca6e9a8c019
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-10 00:14:45 -0600

    update detail contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/movie_detail/MovieDetailContract.kt
@@ -0,0 +1,25 @@
+package com.nsutanto.popularmovies.ui.movie_detail
+
+import com.nsutanto.popularmovies.data.model.CreditResponse
+import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.data.model.MovieVideosResponse
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+import com.nsutanto.popularmovies.viewmodel.MovieDetailViewModel
+
+interface MovieDetailContract {
+
+    interface View {
+        fun showMovieDetail(movie: Movie)
+
+        fun showVideos(videosResponse: MovieVideosResponse)
+
+        fun showCredits(creditResponse: CreditResponse)
+
+        fun getViewModel(): MovieDetailViewModel
+    }
+    interface Presenter : BasePresenter {
+        fun onUpdatedVideos(movieVideos: MovieVideosResponse)
+
+        fun onUpdatedCredits(credits: CreditResponse)
+    }
+}
