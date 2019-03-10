eeda048687d960bca5721984330497bd4a43b0db
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-10 00:15:04 -0600

    update detail presenter

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/movie_detail/MovieDetailPresenter.kt
@@ -0,0 +1,35 @@
+package com.nsutanto.popularmovies.ui.movie_detail
+
+import com.nsutanto.popularmovies.data.model.CreditResponse
+import com.nsutanto.popularmovies.data.model.Movie
+import com.nsutanto.popularmovies.data.model.MovieVideosResponse
+import com.nsutanto.popularmovies.viewmodel.MovieDetailViewModel
+import javax.inject.Inject
+
+class MovieDetailPresenter
+    @Inject
+    constructor(private val view: MovieDetailContract.View,
+            private val movie: Movie) : MovieDetailContract.Presenter {
+
+    private var viewModel: MovieDetailViewModel? = null
+
+    override fun start() {
+        viewModel = view.getViewModel()
+        viewModel?.getMovieVideos()
+        viewModel?.getMovieCredits()
+
+        view.showMovieDetail(movie)
+    }
+
+    override fun stop() {
+        viewModel?.clear()
+    }
+
+    override fun onUpdatedVideos(videosResponse: MovieVideosResponse) {
+        view.showVideos(videosResponse)
+    }
+
+    override fun onUpdatedCredits(credits: CreditResponse) {
+        view.showCredits(credits)
+    }
+}
