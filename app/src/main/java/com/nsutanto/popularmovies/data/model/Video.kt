6ecadeb692fd9e46f74e0582351b94bc8c5c19ef
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-10 00:16:38 -0600

    Update Video

--- app/src/main/java/com/nsutanto/popularmovies/data/model/Video.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/model/Video.kt
@@ -7,13 +7,16 @@ import com.nsutanto.popularmovies.utils.AppConstants.YOUTUBE_URL
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Video(val id: String) : Parcelable {
+data class Video(val id: String?,
+                 val name: String?,
+                 val type: String?,
+                 val key: String?) : Parcelable {
 
     fun getYouTubeURL(): String {
-        return YOUTUBE_URL + id
+        return YOUTUBE_URL + key
     }
 
     fun getYouTubeThumbnailURL(): String {
-        return YOUTUBE_THUMBNAIL_PRE + id + YOUTUBE_THUMBNAIL_POST
+        return YOUTUBE_THUMBNAIL_PRE + key + YOUTUBE_THUMBNAIL_POST
     }
 }
