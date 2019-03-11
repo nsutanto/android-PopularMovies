06a51b5332ac18c67a8c1175b0b9c77e440c12eb
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-11 08:19:49 -0500

    Update Service

--- app/src/main/java/com/nsutanto/popularmovies/data/api/TMDBService.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/api/TMDBService.kt
@@ -1,8 +1,6 @@
 package com.nsutanto.popularmovies.data.api
 
-import com.nsutanto.popularmovies.data.model.MovieResponse
-import com.nsutanto.popularmovies.data.model.Review
-import com.nsutanto.popularmovies.data.model.Video
+import com.nsutanto.popularmovies.data.model.*
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -17,21 +15,25 @@ interface TMDBService {
     fun getTopRatedMovie(): Observable<MovieResponse>
 
     @GET("movie/{id}/videos")
-    fun getMovieVideos(@Path("id") id: String): Observable<Video>
+    fun getMovieVideos(@Path("id") id: Int): Observable<MovieVideosResponse>
 
     @GET("movie/{id}/reviews")
-    fun getMovieReviews(@Path("id") id: String): Observable<Review>
+    fun getMovieReviews(@Path("id") id: Int): Observable<Review>
+
+    @GET("movie/{id}/credits")
+    fun getMovieCredit(@Path("id") id: Int): Observable<CreditResponse>
 
     // TV
     @GET("tv/popular")
-    fun getPopularTV(): Observable<MovieResponse>
+    fun getPopularTV(): Observable<TVResponse>
 
     @GET("tv/top_rated")
-    fun getTopRatedTV(): Observable<MovieResponse>
+    fun getTopRatedTV(): Observable<TVResponse>
 
     @GET("tv/{id}/videos")
-    fun getTVVideos(@Path("id") id: String): Observable<Video>
+    fun getTVVideos(@Path("id") id: Int): Observable<MovieVideosResponse>
 
     @GET("tv/{id}/reviews")
-    fun getTVReviews(@Path("id") id: String): Observable<Review>
+    fun getTVReviews(@Path("id") id: Int): Observable<Review>
+
 }
