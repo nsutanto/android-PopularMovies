c52bba72de64c15917bf86e08a6ad6a84e59d447
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-12 07:53:14 -0500

    update movie

--- app/src/main/java/com/nsutanto/popularmovies/data/model/Movie.kt
+++ app/src/main/java/com/nsutanto/popularmovies/data/model/Movie.kt
@@ -4,11 +4,12 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Movie(val id: Int,
-                 val title: String,
-                 val releaseDate: String,
-                 val posterPath: String,
-                 val voteAverage: String,
-                 val overview: String,
-                 val favorite: Int) : Parcelable
+data class Movie(val id: Int?,
+                 val title: String?,
+                 val releaseDate: String?,
+                 val posterPath: String?,
+                 val backdropPath: String?,
+                 val voteAverage: String?,
+                 val overview: String?,
+                 val favorite: Int?) : Parcelable
 
