74a54906f2d8cbcb59ffa464d9c64e37fb1f5700
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-18 09:32:06 -0500

    remove main activity listener

--- app/src/main/java/com/nsutanto/popularmovies/ui/main/MainActivityListener.kt
+++
@@ -1,8 +0,0 @@
-package com.nsutanto.popularmovies.ui.main
-
-import com.nsutanto.popularmovies.data.model.Movie
-
-interface MainActivityListener {
-
-    fun displayPopularMovies(movies: List<Movie>)
-}
