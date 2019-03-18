df87884ea5199ae6f8b45a939ca41a8081f2023b
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-18 09:31:39 -0500

    update app constant

--- app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
+++ app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
@@ -4,11 +4,16 @@ import com.nsutanto.popularmovies.BuildConfig
 
 object AppConstants {
 
-    const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/w185/"
+    const val BASE_URL_POSTER = "https://image.tmdb.org/t/p/w185"
+    const val BASE_URL_POSTER_MEDIUM = "https://image.tmdb.org/t/p/w342"
+    const val BASE_URL_POSTER_BIG = "https://image.tmdb.org/t/p/w500"
+    const val BASE_URL_BACKDROP_MEDIUM = "https://image.tmdb.org/t/p/h632"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val YOUTUBE_URL = "https://www.youtube.com/watch?v="
-    const val YOUTUBE_THUMBNAIL_PRE = "http://img.youtube.com/vi/"
+    const val YOUTUBE_THUMBNAIL_PRE = "https://img.youtube.com/vi/"
     const val YOUTUBE_THUMBNAIL_POST = "/0.jpg"
     const val API_KEY = "api_key"
     const val API_KEY_VALUE = BuildConfig.API_KEY
+
+    const val INVALID_ACTIVITY = "Invalid Activity"
 }
