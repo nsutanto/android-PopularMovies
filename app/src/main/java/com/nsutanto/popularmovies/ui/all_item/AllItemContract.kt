9756030177de525b9e898df1e70dda309ff64e05
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-25 07:50:19 -0500

    create all item contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/all_item/AllItemContract.kt
@@ -0,0 +1,10 @@
+package com.nsutanto.popularmovies.ui.all_item
+
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+
+interface AllItemContract {
+
+    interface View
+
+    interface Presenter : BasePresenter
+}
