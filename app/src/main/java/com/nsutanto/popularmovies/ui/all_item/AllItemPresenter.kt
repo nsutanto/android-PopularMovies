12f040959347dcde487b4f3cad41e86afd9bbb44
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-25 07:50:27 -0500

    create all item presenter

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/all_item/AllItemPresenter.kt
@@ -0,0 +1,17 @@
+package com.nsutanto.popularmovies.ui.all_item
+
+import javax.inject.Inject
+
+class AllItemPresenter
+@Inject
+constructor(private val view: AllItemContract.View) : AllItemContract.Presenter {
+
+    override fun create() {
+    }
+
+    override fun start() {
+    }
+
+    override fun stop() {
+    }
+}
