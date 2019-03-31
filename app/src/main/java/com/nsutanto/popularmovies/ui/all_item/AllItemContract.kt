6b7c13cba801cb9ba55bae07066878cba3992468
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-03-31 16:37:34 -0500

    add all item contract

--- app/src/main/java/com/nsutanto/popularmovies/ui/all_item/AllItemContract.kt
+++ app/src/main/java/com/nsutanto/popularmovies/ui/all_item/AllItemContract.kt
@@ -1,10 +1,25 @@
 package com.nsutanto.popularmovies.ui.all_item
 
+import com.nsutanto.popularmovies.data.model.MovieResponse
 import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+import com.nsutanto.popularmovies.viewmodel.MainViewModel
 
 interface AllItemContract {
 
-    interface View
+    interface View {
+        fun getViewModel() : MainViewModel
 
-    interface Presenter : BasePresenter
+        fun setItems(objects: List<Any>?)
+    }
+
+    interface Presenter : BasePresenter {
+
+        fun onUpdatedPopularMovies(movieResponse: MovieResponse)
+
+        fun onUpdatedTopRatedMovies(movieResponse: MovieResponse)
+
+        fun fetchPopularMovies()
+
+        fun fetchTopRatedMovies()
+    }
 }
