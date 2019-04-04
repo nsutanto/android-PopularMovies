42ba9ad2cdb75faeea395c5f159b35e40b42d0f8
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-04-04 07:48:36 -0500

    add tv detail contract

---
+++ app/src/main/java/com/nsutanto/popularmovies/ui/tv_detail/TVDetailContract.kt
@@ -0,0 +1,25 @@
+package com.nsutanto.popularmovies.ui.tv_detail
+
+import com.nsutanto.popularmovies.data.model.CreditResponse
+import com.nsutanto.popularmovies.data.model.MovieVideosResponse
+import com.nsutanto.popularmovies.data.model.TV
+import com.nsutanto.popularmovies.ui.base.presenter.BasePresenter
+import com.nsutanto.popularmovies.viewmodel.MovieDetailViewModel
+
+interface TVDetailContract {
+
+    interface View {
+        fun showTVDetail(tv: TV)
+
+        fun showVideos(videosResponse: MovieVideosResponse)
+
+        fun showCredits(creditResponse: CreditResponse)
+
+        fun getViewModel(): MovieDetailViewModel
+    }
+    interface Presenter : BasePresenter {
+        fun onUpdatedVideos(videosResponse: MovieVideosResponse)
+
+        fun onUpdatedCredits(credits: CreditResponse)
+    }
+}
