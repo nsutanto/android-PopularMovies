cf4d27190537bdaa9c95422a6ddb7607dda92d5e
Nicholas Sutanto <nicholas.sutanto@gmail.com>
2019-04-05 12:32:40 -0500

    update app constant

--- app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
+++ app/src/main/java/com/nsutanto/popularmovies/utils/AppConstants.kt
@@ -16,4 +16,23 @@ object AppConstants {
     const val API_KEY_VALUE = BuildConfig.API_KEY
 
     const val INVALID_ACTIVITY = "Invalid Activity"
+
+
+    const val MOVIE_INTENT = ".movie"
+    const val MOVIE_LIST_INTENT = ".movie_list"
+    const val TV_INTENT = ".tv"
+    const val TV_LIST_INTENT = ".tv_list"
+    const val ALL_ITEM_TYPE_INTENT = ".allItemType"
+
+    var POPULAR_MOVIE_PAGE = 1
+    var TOP_RATED_MOVIE_PAGE = 1
+    var POPULAR_TV_PAGE = 1
+    var TOP_RATED_TV_PAGE = 1
+
+    enum class AllItemType(val value: Int) {
+        POPULAR_MOVIE(0),
+        TOP_RATED_MOVIE(1),
+        POPULAR_TV(2),
+        TOP_RATED_TV(3)
+    }
 }
